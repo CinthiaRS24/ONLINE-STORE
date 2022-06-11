0b23d8f97852c102527de9c5dbb97b5e9fcb659e
Cinthia Ramos <cinthia24027@gmail.com>
2022-06-11 11:49:02 -0500

    a single route for all products and products by category and pagination from the back

--- BACK/src/routes/index.js
+++ BACK/src/routes/index.js
@@ -7,73 +7,232 @@ const { Product, Category } = require('../db')
 
 const categories = async () => {
     let categories = await Category.findAll()
-
     categories = JSON.parse(JSON.stringify(categories));
-
     return categories;
 }
 
-const allProducts = async () => {
-    let allProducts = await Product.findAll()
+const allProducts = async (page, orderByName, orderByPrice) => {
+    const resgistropp = 6;
+    const desde = page * resgistropp;
+    
 
-    allProducts = JSON.parse(JSON.stringify(allProducts));
-
-    return allProducts;
-}
-
-const allProductsOrderByName = async (orderByName) => {
-    let allProducts = await Product.findAll({
-        order: [
-            ["name", `${orderByName}`],
-        ],
-    })
-
-    allProducts = JSON.parse(JSON.stringify(allProducts));
-
-    return allProducts;
-}
-
-const allProductsOrderByPrice = async (orderByPrice) => {
-    let allProducts = await Product.findAll({
-        order: [
-            ["price", `${orderByPrice}`],
-        ],
-    })
-
-    allProducts = JSON.parse(JSON.stringify(allProducts));
-
-    return allProducts;
-}
-   
-const productsByCategory = async (id) => {
-    let productsByCategory = await Product.findAll({
-        where: {
-            category: id
-        }
-    })
-
-    productsByCategory = JSON.parse(JSON.stringify(productsByCategory));
-
-    return productsByCategory;
-}
-
-
-const productsByName = async (name) => {
-    let productsByName = await Product.findAll({
-        where: {
-            name: {
-                [Op.like]: '%' + name + '%'
-            }
-        },
-    })
-
-    productsByName = JSON.parse(JSON.stringify(productsByName));
-
-    if(productsByName.length <= 0) {
-        return "No results";
-    } else {
-        return productsByName;
+    if(!orderByName && !orderByPrice) {
+        let products = await Product.findAll({ offset: desde, limit: resgistropp });
+        const total = await Product.count();
+        //allProducts = JSON.parse(JSON.stringify(allProducts));
+        return {
+            status: 'success',
+            page: {
+                desde,
+                resgistropp,
+                total
+            },
+            products
+        };
     }
+
+    if(orderByName) {
+        let products = await Product.findAll({
+            order: [
+                ["name", `${orderByName}`],
+            ],
+            offset: desde, 
+            limit: resgistropp
+        })
+        const total = await Product.count();
+        //allProducts = JSON.parse(JSON.stringify(allProducts));
+        return {
+            status: 'success',
+            page: {
+                desde,
+                resgistropp,
+                total
+            },
+            products
+        };
+    } else {
+        let products = await Product.findAll({
+            order: [
+                ["price", `${orderByPrice}`],
+            ],
+            offset: desde, 
+            limit: resgistropp
+        })
+        const total = await Product.count();
+        //allProducts = JSON.parse(JSON.stringify(allProducts));
+        return {
+            status: 'success',
+            page: {
+                desde,
+                resgistropp,
+                total
+            },
+            products
+        };
+    }
+}
+
+
+
+const productsByCategory = async (page, id, orderByName, orderByPrice) => {
+    const resgistropp = 6;
+    const desde = page * resgistropp;
+
+    if(!orderByName && !orderByPrice) {
+        let products  = await Product.findAndCountAll({
+            where: {
+                category: id
+            },
+            offset: desde, 
+            limit: resgistropp
+        })
+        //productsByCategory = JSON.parse(JSON.stringify(productsByCategory));
+        return {
+            status: 'success',
+            page: {
+                desde,
+                resgistropp,
+                count: products.count
+            },
+            rows: products.rows
+        };
+    }
+
+    if(orderByName) {
+        let products  = await Product.findAndCountAll({
+            where: {
+                category: id
+            },
+            order: [
+                ["name", `${orderByName}`],
+            ],
+            offset: desde, 
+            limit: resgistropp
+        })
+        //allProducts = JSON.parse(JSON.stringify(allProducts));
+        return {
+            status: 'success',
+            page: {
+                desde,
+                resgistropp,
+                count: products.count
+            },
+            rows: products.rows
+        };
+    } else {
+        let products  = await Product.findAndCountAll({
+            where: {
+                category: id
+            },
+            order: [
+                ["price", `${orderByPrice}`],
+            ],
+            offset: desde, 
+            limit: resgistropp
+        })
+        //allProducts = JSON.parse(JSON.stringify(allProducts));
+        return {
+            status: 'success',
+            page: {
+                desde,
+                resgistropp,
+                count: products.count
+            },
+            rows: products.rows
+        };
+    }
+
+    
+}
+
+
+const productsByName = async (page, name, orderByName, orderByPrice) => {
+    const resgistropp = 6;
+    const desde = page * resgistropp;
+
+    if(!orderByName && !orderByPrice) {
+        let productsByName = await Product.findAndCountAll({
+            where: {
+                name: {
+                    [Op.like]: '%' + name + '%'
+                }
+            },
+            offset: desde, 
+            limit: resgistropp
+        })
+        //allProducts = JSON.parse(JSON.stringify(allProducts));
+        if(productsByName.count <= 0) {
+            return "No results";
+        } else {
+            return {
+                status: 'success',
+                page: {
+                    desde,
+                    resgistropp,
+                    count: productsByName.count
+                },
+                rows: productsByName.rows
+            };
+        }
+    }
+
+
+    if(orderByName) {
+        let productsByName = await Product.findAndCountAll({
+            where: {
+                name: {
+                    [Op.like]: '%' + name + '%'
+                }
+            },
+            order: [
+                ["name", `${orderByName}`],
+            ],
+            offset: desde, 
+            limit: resgistropp
+        })
+
+        if(productsByName.count <= 0) {
+            return "No results";
+        } else {
+            return {
+                status: 'success',
+                page: {
+                    desde,
+                    resgistropp,
+                    count: productsByName.count
+                },
+                rows: productsByName.rows
+            };
+        }
+    } else {
+        let productsByName = await Product.findAndCountAll({
+            where: {
+                name: {
+                    [Op.like]: '%' + name + '%'
+                }
+            },
+            order: [
+                ["price", `${orderByPrice}`],
+            ],
+            offset: desde, 
+            limit: resgistropp
+        })
+        //allProducts = JSON.parse(JSON.stringify(allProducts));
+        if(productsByName.count <= 0) {
+            return "No results";
+        } else {
+            return {
+                status: 'success',
+                page: {
+                    desde,
+                    resgistropp,
+                    count: productsByName.count
+                },
+                rows: productsByName.rows
+            };
+        }
+    }
+
 
 }
 
@@ -89,37 +248,30 @@ router.get('/categories', async (req, res) => {
 
 // Para traer todos los productos
 router.get('/', async (req, res) => {
+    const page = Number(req.query.page) || 0;
+    const {id} = req.query;
     const {orderByName} = req.query;
     const {orderByPrice} = req.query;
 
-
-    if(!orderByName && !orderByPrice) {
-        const result = await allProducts();
-        res.json(result);
+    if(id) {
+        const result = await productsByCategory(page, id, orderByName, orderByPrice)
+        return res.json(result);
+    } else {
+        const result = await allProducts(page, orderByName, orderByPrice)
+        return res.json(result);
     }
-
-    const result = orderByName? await allProductsOrderByName(orderByName) : await allProductsOrderByPrice(orderByPrice);
-    
-    console.log('result', result)
-    res.json(result);
 })
 
-// Para traer los productos por categoría
-router.get('/productsByCategory', async (req, res) => {
-    const {id} = req.query;
-    console.log('query', req.query);
-    console.log('id', id)
-    const result = await productsByCategory(id);
-    // console.log('result', result)
-    res.json(result);
-})
+
 
 // Para buscar productos por nombre
 router.get('/products', async (req, res) => {
     const {name} = req.query;
-    console.log('name', name)
-    const result = await productsByName(name);
-    // console.log('result', result)
+    const page = Number(req.query.page) || 0;
+    const {orderByName} = req.query;
+    const {orderByPrice} = req.query;
+
+    const result = await productsByName(page, name, orderByName, orderByPrice);
     res.json(result);
 })
 
