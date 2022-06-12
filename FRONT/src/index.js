900a9c6716c94aa2f8795a63f861d698b1f2d637
Cinthia Ramos <cinthia24027@gmail.com>
2022-06-11 20:43:36 -0500

    tidy frontend code

--- FRONT/src/index.js
+++ FRONT/src/index.js
@@ -8,37 +8,6 @@ function productHtml (product) {
 }
 
 
-async function categories() {
-    let result = document.getElementById('first-column');
-
-    await fetch('http://localhost:3050/categories')
-        .then(r => r.json())
-        .then((response) => {
-            response.forEach((c) =>{
-                const newBtn = `<button id="${c.id}">${c.name}</button>`
-                result.innerHTML += newBtn
-            })
-        })
-        .then(() => {
-            const botones = Array.from(document.getElementById('first-column').children);
-            botones.forEach(button => {
-                button.addEventListener('click', (e) => {
-                    nameOid = `&id=${e.target['id']}`
-                    link = `http://localhost:3050/products?page=${page}${nameOid}${orderBy}`
-                    main(link);
-                    console.log('Link', link)
-                } 
-            )})
-           
-        })
-        .catch(error => 
-            console.log(error));   
-}
-
-categories();
-
-
-
 let page = 0;
 let nameOid = "&";
 let orderBy = "&";
@@ -52,18 +21,14 @@ function main(link) {
     const btnsPagination = document.getElementById('pagination')
     btnsPagination.innerHTML = "";
 
-    console.log('link', link)
     fetch(link)
         .then(r => r.json())
         .then((response) => {
-            console.log('response', response)
-
             response.rows.forEach((p) =>{
                 let newDiv = productHtml(p);
                 result.innerHTML += newDiv
             })   
 
-           
             const total = response.page.count;
             const pageNumbers = [];
             for (let i = 0; i < Math.ceil(total/6); i++) {
@@ -78,66 +43,81 @@ function main(link) {
             botones.forEach(button => {
                 button.addEventListener('click', (e) => {
                 clickBtn(`${e.target.id - 1}`);
-            })
+                })
             }) 
         })
-
+        .catch(error => {
+            console.log(error)
+        });
 }
 
 main(link)
 
 
-function clickBtn(number) {
-    page = number;
-    console.log('btnpage', page)
-    link = `http://localhost:3050/products?page=${page}${nameOid}${orderBy}`
-    main(link);
-    console.log('Link2', link)
-}
+async function categories() {
+    let result = document.getElementById('first-column');
 
-
-function productsByName(name) {
-    // Ocultar el contenido del div
-    let result = document.getElementById('result');
-    result.innerHTML = "";
-    let allProducts = [];
-
-    fetch(`http://localhost:3050/products?name=${name}`)
+    await fetch('http://localhost:3050/categories')
         .then(r => r.json())
         .then((response) => {
-            if(!Array.isArray(response)) result.innerHTML = "<p>No results</p>";
-
-            response.forEach((p) =>{
-                let newDiv = productHtml(p);
-                // result.innerHTML += newDiv;
-                allProducts.push(newDiv);
+            response.forEach((c) =>{
+                const newBtn = `<button id="${c.id}">${c.name}</button>`;
+                result.innerHTML += newBtn;
             })
         })
         .then(() => {
-            pagination(allProducts);
+            const botones = Array.from(document.getElementById('first-column').children);
+            botones.forEach(button => {
+                button.addEventListener('click', (e) => {
+                    nameOid = `&id=${e.target['id']}`;
+                    link = `http://localhost:3050/products?page=${page}${nameOid}${orderBy}`;
+                    main(link);
+                }) 
+            })
         })
-        .catch(error => 
-            console.log(error));  
+        .catch(error => {
+            console.log(error)
+        });   
+}
+
+categories();
+
+
+function clickBtn(number) {
+    page = number;
+    link = `http://localhost:3050/products?page=${page}${nameOid}${orderBy}`
+    main(link);
 }
 
 
-// Botón reset
-const reset = document.getElementById('reset');
-console.log('reset', reset)
-reset.addEventListener('click', (e) => {
-    page = 0;
-    nameOid = "&";
-    orderBy = "&";
-    link = `http://localhost:3050/products?page=${page}${nameOid}${orderBy}`
-    main(link);
-    console.log('Link', link)
-})
+
+// Botones ordenar por nombre
+const btnOrderByName = Array.from(document.getElementById('btns-orderName').children);
+btnOrderByName.forEach(button => {
+    button.addEventListener('click', (e) => {
+        orderBy = `&orderByName=${e.target.innerHTML}`
+        link = `http://localhost:3050/products?page=${page}${nameOid}${orderBy}`
+        main(link);
+        console.log('Link', link)
+    } 
+)})
 
 
-// Botón search
+// Botones ordenar por precio
+const btnOrderByPrice = Array.from(document.getElementById('btns-orderPrice').children);
+btnOrderByPrice.forEach(button => {
+    button.addEventListener('click', (e) => {
+        orderBy = `&orderByPrice=${e.target.innerHTML}`
+        link = `http://localhost:3050/products?page=${page}${nameOid}${orderBy}`
+        main(link);
+        console.log('Link', link)
+    } 
+)})
+
+
+// Botón Search
 const search = document.getElementById('input-search');
 search.addEventListener('input', updateValue)
-console.log('search1', search.value)
 
 function updateValue(e) {
     search.value = e.target.value;
@@ -149,29 +129,16 @@ btnSearch.addEventListener('click', (e) => {
     nameOid = `&name=${search.value}`;
     link = `http://localhost:3050/products?page=${page}${nameOid}${orderBy}`
     main(link);
-    console.log('Link', link)
 })
 
 
-
-
-
-const btnOrderByName = Array.from(document.getElementById('btns-orderName').children);
-btnOrderByName.forEach(button => {
-    button.addEventListener('click', (e) => {
-        orderBy = `&orderByName=${e.target.innerHTML}`
-        link = `http://localhost:3050/products?page=${page}${nameOid}${orderBy}`
-        main(link);
-        console.log('Link', link)
-    } 
-)})
-
-const btnOrderByPrice = Array.from(document.getElementById('btns-orderPrice').children);
-btnOrderByPrice.forEach(button => {
-    button.addEventListener('click', (e) => {
-        orderBy = `&orderByPrice=${e.target.innerHTML}`
-        link = `http://localhost:3050/products?page=${page}${nameOid}${orderBy}`
-        main(link);
-        console.log('Link', link)
-    } 
-)})
+// Botón Reset
+const reset = document.getElementById('reset');
+reset.addEventListener('click', (e) => {
+    page = 0;
+    nameOid = "&";
+    orderBy = "&";
+    link = `http://localhost:3050/products?page=${page}${nameOid}${orderBy}`
+    main(link);
+    search.value = "";
+})
