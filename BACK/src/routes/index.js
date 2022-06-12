752472c15a26d62f7096e3c215f720c8526ea9c5
Cinthia Ramos <cinthia24027@gmail.com>
2022-06-11 20:26:57 -0500

    tidy backend code

--- BACK/src/routes/index.js
+++ BACK/src/routes/index.js
@@ -1,8 +1,8 @@
-// const app = require("../app");
 const { Router } = require('express');
 const { Op } = require('sequelize');
 const router = Router();
 const { Product, Category } = require('../db')
+
 
 
 const categories = async () => {
@@ -13,58 +13,57 @@ const categories = async () => {
 
 
 const allProducts = async (page, id, name, orderByName, orderByPrice) => {
-    const resgistropp = 6;
-    const desde = page * resgistropp;
+    const productsPerPage = 6;
+    const desde = page * productsPerPage;
 
     const category = id? {category: Number(id)} : name? {
         name: {
             [Op.like]: '%' + name + '%'
         }
     } : {}
-    console.log('category', category)
 
     const orderBy = orderByName? [["name", `${orderByName}`]] : orderByPrice? [["price", `${orderByPrice}`]] : []
 
-    console.log('orderBy', orderBy)
     
-        let products = await Product.findAndCountAll({
-            where: category, 
-            order: orderBy,
-            offset: desde, 
-            limit: resgistropp
-        });
-        return {
-            status: 'success',
-            page: {
-                desde,
-                resgistropp,
-                count: products.count
-            },
-            rows: products.rows
-        };
+    let products = await Product.findAndCountAll({
+        where: category, 
+        order: orderBy,
+        offset: desde, 
+        limit: productsPerPage
+    });
+
+    return {
+        status: 'success',
+        page: {
+            desde,
+            productsPerPage,
+            count: products.count
+        },
+        rows: products.rows
+    };
 }
 
 
 
-// Para traer las categorías
+// Ruta para traer las categorías
 router.get('/categories', async (req, res) => {
     const result = await categories();
     console.log('result', result)
     res.json(result);
 })
 
-// Para traer todos los productos
+// Ruta para traer los productos solicitados
 router.get('/products', async (req, res) => {
     const page = Number(req.query.page) || 0;
     const {id} = req.query;
     const {name} = req.query
     const {orderByName} = req.query;
     const {orderByPrice} = req.query;
-
     
-        const result = await allProducts(page, id, name, orderByName, orderByPrice)
-        return res.json(result);
-    
+    const result = await allProducts(page, id, name, orderByName, orderByPrice)
+    return res.json(result);
 })
+
+
 
 module.exports = router;
